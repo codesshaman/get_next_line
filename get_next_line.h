ecc74b6980169220678e1aef5893662be8ef92a9
Jimmie Leslee <jleslee@ge-m1.kzn.21-school.ru>
2021-11-23 19:53:30 +0300

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleslee <jleslee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 21:12:16 by jleslee           #+#    #+#             */
+/*   Updated: 2021/11/23 18:36:32 by jleslee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+# define BUFFSIZE 50
+
+#endif
